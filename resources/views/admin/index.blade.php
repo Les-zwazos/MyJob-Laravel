f59ee2e106440fe6abbf59ce15cfc54c60602b03
LeGrandF38 <faycalyacoubaprog@gmail.com>
2024-03-02 09:12:10 +0100

    add text de bienvenu sur admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1>Page admin</h1>
+                    <h1>Bienvenue sur My Job !<br><br><br><br>
+
+                        Nous sommes ravis de vous accueillir dans notre communauté dédiée à l'emploi et au recrutement. My Job est conçu pour vous aider à trouver les meilleures opportunités professionnelles et à recruter les talents les plus qualifiés.<br><br>
+                        
+                        En tant qu'utilisateur de l'interface admin, vous avez accès à un ensemble d'outils puissants pour gérer vos offres d'emploi, suivre les candidatures, et interagir avec les candidats. Explorez nos fonctionnalités et n'hésitez pas à nous contacter si vous avez besoin d'aide ou de conseils.<br><br>
+                        
+                        Nous vous souhaitons beaucoup de succès dans votre gestion, et espérons que My Job deviendra un outil indispensable pour vous dans votre activité professionnel.<br><br><br>
+                        
+                        L'équipe My Job<br></h1>
                 </div>
             </div>
         </div>
